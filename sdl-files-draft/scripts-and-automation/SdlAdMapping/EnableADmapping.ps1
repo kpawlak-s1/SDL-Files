f8e8a6cd3071db40b9aaffb1f4b77671bebfc057
kpawlak-s1 <142378353+kpawlak-s1@users.noreply.github.com>
2024-04-30 00:10:03 -0700

    Update EnableADmapping.ps1

--- sdl-files-draft/scripts-and-automation/SdlAdMapping/EnableADmapping.ps1
+++ sdl-files-draft/scripts-and-automation/SdlAdMapping/EnableADmapping.ps1
@@ -1,13 +1,3 @@
-$EnableADMappingurl = "https://github.com/kpawlak-s1/SDL-Files/blob/main/sdl-files-draft/scripts-and-automation/GatherADMapping.ps1"
-$EnableADMappingoutputFile = "EnableADmapping.ps1"
 
-Invoke-WebRequest -Uri $EnableADMappingurl -OutFile $EnableADMappingoutputFile
-
-$UploadADMappingtoSDLurl = "https://github.com/kpawlak-s1/SDL-Files/blob/main/sdl-files-draft/scripts-and-automation/UploadADMappingtoSDL.ps1"
-$UploadADMappingtoSDLoutputFile = "UploadADMappingtoSDL.ps1"
-
-Invoke-WebRequest -Uri $nUploadADMappingtoSDL -OutFile $UploadADMappingtoSDL
-
-
-schtasks /create /tn "RunPowerShellScript" /tr "PowerShell.exe -File .\GatherADMapping.ps1" /sc daily /mo 1 /st 00:01
+schtasks /create /tn "RunPowerShellScript" /tr "PowerShell.exe -File .\GatherADMapping.ps1 > ADMapping.json" /sc daily /mo 1 /st 00:01
 schtasks /create /tn "RunPowerShellScript" /tr "PowerShell.exe -File .\UploadADMappingtoSDL.ps1" /sc daily /mo 1 /st 00:05
