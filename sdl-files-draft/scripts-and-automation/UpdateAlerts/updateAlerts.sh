c1580b371b2292d89148d2ee9f7247ff3f09552c
kpawlak-s1 <142378353+kpawlak-s1@users.noreply.github.com>
2024-04-30 12:30:33 -0700

    update alerts scripting

---
+++ sdl-files-draft/scripts-and-automation/UpdateAlerts/updateAlerts.sh
@@ -0,0 +1,8 @@
+curl --location 'https://xdr.us1.sentinelone.net//getFile' \
+--header 'Authorization: Bearer 0HuQ4SSXeO3dfoTUpOlbO3SoHCDqqlaGnVJTH8s2/Z4E-' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: sp=3156b153-fa63-415c-910a-0a521f6eab06' \
+--data '{
+    "token": "0HuQ4SSXeO3dfoTUpOlbO3SoHCDqqlaGnVJTH8s2/Z4E-",
+    "path": "/scalyr/alerts",
+}'
