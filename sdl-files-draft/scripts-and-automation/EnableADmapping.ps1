6a41c2c954071acb87e2f5c58db244b11f3411b4
kpawlak-s1 <142378353+kpawlak-s1@users.noreply.github.com>
2024-04-30 00:02:45 -0700

    updating folder structure

--- sdl-files-draft/scripts-and-automation/EnableADmapping.ps1
+++
@@ -1,2 +0,0 @@
-schtasks /create /tn "RunPowerShellScript" /tr "PowerShell.exe -File .\GatherADMapping.ps1" /sc daily /mo 1 /st 00:01
-schtasks /create /tn "RunPowerShellScript" /tr "PowerShell.exe -File .\UploadADMappingtoSDL.ps1" /sc daily /mo 1 /st 00:05
