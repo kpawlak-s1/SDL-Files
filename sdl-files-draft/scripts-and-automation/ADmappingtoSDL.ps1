01cbb8061a52e750a5cdd6608ecdb038ba4cdd9f
kpawlak-s1 <142378353+kpawlak-s1@users.noreply.github.com>
2024-04-29 23:43:32 -0700

    Update ADmappingtoSDL.ps1

--- sdl-files-draft/scripts-and-automation/ADmappingtoSDL.ps1
+++ sdl-files-draft/scripts-and-automation/ADmappingtoSDL.ps1
@@ -1,20 +1,28 @@
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
+
 # Get a list of all users
 $users = Get-ADUser -Filter *
+
+$columnNamesString = '{ columnNames: ["UserName","Groups"],'
+$columnNamesString 
+$rowString = 'rows: ['
+$rowString
+
 
 # Loop through each user
 foreach ($user in $users) {
     # Get the groups the user is a member of
     $userGroups = Get-ADPrincipalGroupMembership -Identity $user | Select-Object -ExpandProperty Name
     
-    # Create an object with user's information and groups
-    $userObject = [PSCustomObject]@{
-        UserName = $user.Name
-        Groups = $userGroups -join ', '
-    }
-    
     # Output the object to JSON
-    $userObject | ConvertTo-Json
-}
+    $formattedUserName = $user.SamAccountName
+    $formattedUserGroupObject = "[ '$formattedUserName','$userGroups' ],"
+    $formattedString2 = $formattedUserGroupObject -replace "'", '"'
+    $formattedString2
+    }
+
+$endString = ']}'
+$endString
+
